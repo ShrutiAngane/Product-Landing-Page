fff2bf8fb96ca7174df4262825df56407c2d7036
Shruti Angane <shrutiangane628@gmail.com>
2023-02-24 00:06:08 +0530

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,8 +13,8 @@ const Navbar = () => {
   return (
     <>
     <header className='absolute flex flex-row justify-end items-center min-w-full lg:w-[875px] h-[91px] xl:w-[1057px] z-20'>
-      <h1 className={`${hamburger?'text-[#737373] text-[20px]':'text-[#FFFFFF] text-[24px]'} font-bold absolute max-[1204px]:left-[30px]`}>Physionic</h1>
-      <nav className='hidden lg:flex lg:flex-row absolute lg:h-[91px] xl:w-[800px] lg:w-[641px] justify-between items-center lg:left-[250px]'>
+      <h1 className={`${hamburger?'text-[#737373] text-[20px]':'text-[#FFFFFF] text-[24px]'} font-bold absolute left-[30px]`}>Physionic</h1>
+      <nav className='hidden lg:flex lg:flex-row absolute lg:h-[91px] xl:w-[800px] lg:w-[641px] justify-between items-center lg:right-[30px]'>
         <ul className='lg:flex-row'>
           <Link to='#' className='text-[#FFFFFF] text-[20px] font-semibold xl:p-[25px] lg:p-[15px]'>Home</Link>
           <Link to='#' className='text-[#FFFFFF] text-[20px] font-semibold xl:p-[25px] lg:p-[15px]'>Product</Link>
